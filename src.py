56c7046b14a31cc0d3f018aee487fddb1b15206a
Jérémie Kalonji <jeremie.kalonji@gmail.com>
2024-11-13 15:45:54 +0100

    First version (very simple answers when not empty)

--- src.py
+++ src.py
@@ -1,5 +1,6 @@
 from llama_index.tools.finance import FinanceAgentToolSpec
-
+from llama_index.agent.openai import OpenAIAgent
+from llama_index.llms.openai import OpenAI
 import os
 
 
@@ -7,8 +8,8 @@ POLYGON_API_KEY = os.environ.get('POLYGON_API_KEY')
 FINNHUB_API_KEY = os.environ.get('FINNHUB_API_KEY')
 NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 ALPHA_VANTAGE_API_KEY = os.environ.get('ALPHA_VANTAGE_API_KEY')
-OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
-
+OPENAI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
+GPT_MODEL_NAME = "gpt-4o-mini"
 
 
 
@@ -20,5 +21,32 @@ tool_spec = FinanceAgentToolSpec(
     )
 
 
+def create_agent(
+    polygon_api_key: str,
+    finnhub_api_key: str,
+    alpha_vantage_api_key: str,
+    newsapi_api_key: str,
+    openai_api_key: str,
+) -> OpenAIAgent:
+    tool_spec = FinanceAgentToolSpec(
+        polygon_api_key,
+        finnhub_api_key,
+        alpha_vantage_api_key,
+        newsapi_api_key,
+    )
+    llm = OpenAI(temperature=0, model=GPT_MODEL_NAME, api_key=openai_api_key)
+    return OpenAIAgent.from_tools(
+        tool_spec.to_tool_list(), llm=llm, verbose=True
+    )
 
-print(tool_spec.spec_functions)
+agent = create_agent(
+    POLYGON_API_KEY,
+    FINNHUB_API_KEY,
+    ALPHA_VANTAGE_API_KEY,
+    NEWS_API_KEY,
+    OPENAI_API_KEY,
+)
+     
+
+response = agent.chat("What was the last closing price of amazon?")
+print(response)
