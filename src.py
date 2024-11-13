d7e51cacf607cea3ec87c418a5fba634c48da23d
Jérémie Kalonji <jeremie.kalonji@gmail.com>
2024-11-13 14:58:24 +0100

    Loading API keys

--- src.py
+++ src.py
@@ -3,4 +3,21 @@ from llama_index.tools.finance import FinanceAgentToolSpec
 import os
 
 
+POLYGON_API_KEY = os.environ.get('POLYGON_API_KEY')
+FINNHUB_API_KEY = os.environ.get('FINNHUB_API_KEY')
+NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+ALPHA_VANTAGE_API_KEY = os.environ.get('ALPHA_VANTAGE_API_KEY')
+OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
+
+
+
+
+tool_spec = FinanceAgentToolSpec(
+        POLYGON_API_KEY,
+        FINNHUB_API_KEY,
+        NEWS_API_KEY,
+        ALPHA_VANTAGE_API_KEY
+    )
+
+
 
